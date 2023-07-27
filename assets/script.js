9d329ed4968a0e0b68388d6022352970a86d98c9
Franklin Membreno <frank_aroca1@hotmail.com>
2023-07-26 16:53:32 -0800

    Updated JavaScirpt code with functions

--- assets/script.js
+++ assets/script.js
@@ -31,10 +31,49 @@ for(var x = 0; x < shapes.length; x++) {
 
 }*/
 
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+//var topic = "HTML";
 
-for(var ind = 0; ind < topics.length; ind++) {
+//var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+/*for(var ind = 0; ind < topics.length; ind++) {
 
     console.log(topics[ind]);
 
+}*/
+
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics() {
+
+    for (var x = 0; x < topics.length; x++) {
+
+        console.log(topics[x]);
+
+    }
+
 }
+
+function selectTopic() {
+   
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
+}
+
+console.log('Here are the topics we learned throug Prework');
+
+listTopics();
+
+console.log('Which topic should we study first?');
+
+selectTopic();
